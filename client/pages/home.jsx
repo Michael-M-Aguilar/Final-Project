54af77bd4b10ebe1f6da0bb1a194f31dd22bc4cf
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-07-21 17:17:46 -0700

    Set up schema.sql, as well as starting the header.

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
 
-export default function Home(props) {
-  return (
-    <>
-    </>
-  );
+export default class DesktopView extends React.Component {
+  render() {
+    return (
+      <div className="flex space-between">
+        <div>
+          Logo
+          {/* <img src={LogoDM}/> */}
+        </div>
+        <div className="flex flex-column-reverse">
+          <div className="flex space-evenly">
+            <div>Home</div>
+            <div>Transactions</div>
+            <div>Folder</div>
+            <div>Chart</div>
+            <div>Settings</div>
+          </div>
+          <div className="flex space-evenly">
+            <span className="logoIcon"><i className="fas fa-home fa-2x"></i></span>
+            <span className="logoIcon"><i className="fas fa-list fa-2x"></i></span>
+            <span className="logoIcon"><i className="fas fa-folder-open fa-2x"></i></span>
+            <span className="logoIcon"><i className="fas fa-chart-pie fa-2x"></i></span>
+            <span className="logoIcon"><i className="fas fa-cog fa-2x"></i></span>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,74 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "users" (
+	"userId" serial NOT NULL,
+	"firstName" TEXT NOT NULL,
+	"lastName" TEXT NOT NULL,
+	"email" TEXT NOT NULL,
+	"password" TEXT NOT NULL,
+	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "entries" (
+	"entryId" serial NOT NULL,
+	"userId" integer NOT NULL,
+	"accountId" integer NOT NULL,
+	"categoryId" integer NOT NULL,
+	"amount" integer NOT NULL,
+	"note" TEXT NOT NULL,
+	"location" TEXT NOT NULL,
+	CONSTRAINT "entries_pk" PRIMARY KEY ("entryId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "accounts" (
+	"accountId" serial NOT NULL,
+	"userId" integer NOT NULL,
+	"type" TEXT NOT NULL,
+	"description" TEXT NOT NULL,
+	CONSTRAINT "accounts_pk" PRIMARY KEY ("accountId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "categories" (
+	"categoryId" serial NOT NULL,
+	"name" serial NOT NULL,
+	CONSTRAINT "categories_pk" PRIMARY KEY ("categoryId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "Budgets" (
+	"budgetId" serial NOT NULL,
+	"userId" integer NOT NULL,
+	"amount" integer NOT NULL,
+	CONSTRAINT "Budgets_pk" PRIMARY KEY ("budgetId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "entries" ADD CONSTRAINT "entries_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
+ALTER TABLE "entries" ADD CONSTRAINT "entries_fk1" FOREIGN KEY ("accountId") REFERENCES "accounts"("accountId");
+ALTER TABLE "entries" ADD CONSTRAINT "entries_fk2" FOREIGN KEY ("categoryId") REFERENCES "Budgets"("categoryId");
+
+ALTER TABLE "accounts" ADD CONSTRAINT "accounts_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
+
+
+ALTER TABLE "Budgets" ADD CONSTRAINT "Budgets_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
