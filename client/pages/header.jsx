c9a0bf0d02768774f2732221b59e0ae67452ed76
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-08-06 15:12:19 -0700

    Fixed the mobile view bug and removed some unncessary classes.

--- client/pages/header.jsx
+++ client/pages/header.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 export default class Header extends React.Component {
   render() {
     return (
-      <div className="container">
+      <div className="">
         <div className="flex space-between">
-          <div>
+          <div className="col col-sm-4 col-md-4">
             <img className="logoSize pt-3" src="/images/logo.png" alt="LogoDM" />
           </div>
-          <div className="flex flex-column header-width py-4">
+          <div className="flex flex-column header-width py-4 col col-sm-8 col-md-8">
             <div className="flex justify-content-between align-items-center">
               <a href="#" className="items-center">
                   <span className="logoIcon ps-1" ><i className="fas fa-home fa-3x"></i></span>
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -4,7 +4,6 @@ import Body from './body';
 export default class Home extends React.Component {
   render() {
     return (
-      // Loads our body inside a container.
       <div className="container">
         <Body />
      </div>
