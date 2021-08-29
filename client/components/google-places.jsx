df47cc90a22c1aec8f14559328a8ed3fad87eceb
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-08-28 23:46:28 -0700

    Checkpoint on the google places.

--- client/components/google-places.jsx
+++ client/components/google-places.jsx
@@ -2,7 +2,7 @@
 // import PlacesAutocomplete from 'react-places-autocomplete';
 
 // function GooglePlaces({ isLoaded, isSuccess }) {
-//   const [address, setAddress] = React.useState('');
+//   const [address, setAddress] = React.useState("");
 
 //   const handleChange = value => {
 //     setAddress(value);
@@ -18,7 +18,27 @@
 //         <PlacesAutocomplete value={address}
 //         onChange={handleChange}
 //         onSelect={handleSelect} >
+//           {({getInputProps, suggestions, getSuggestionItemProps, loading}) => (
+//             <div>
+//               <input {... getInputProps ({
+//                 placeholder: 'Enter...',
+//               })}
+//               />
+//               <div>
+//               {loading && <div>Loading... </div>}
+//               {suggestions.map((suggestion) => {
+//                 return (
+//                   <div {... getSuggestionItemProps(suggestion)}>
+//                     {suggestion.description}
+//                   </div>
+//                 )
+//               })}
+//               </div>
+//             </div>
+//           )}
 //       </div>
 //     )
+//   } else {
+//     return <div></div>
 //   }
 // }
