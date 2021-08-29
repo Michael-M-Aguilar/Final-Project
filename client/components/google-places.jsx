a1cb1f411c1b9a4c64ec11e4ab14f197ff73ce3c
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-08-29 13:43:51 -0700

    Fixing the missing { or ( from the prior code and going over it again.

--- client/components/google-places.jsx
+++ client/components/google-places.jsx
@@ -1,44 +1,29 @@
-// import React from 'react';
-// import PlacesAutocomplete from 'react-places-autocomplete';
+import React from 'react';
+import PlacesAutocomplete from 'react-places-autocomplete';
 
-// function GooglePlaces({ isLoaded, isSuccess }) {
-//   const [address, setAddress] = React.useState("");
+export default function GooglePlaces({ isLoaded, isSuccess }) {
+  const [address, setAddress] = React.useState('');
 
-//   const handleChange = value => {
-//     setAddress(value);
-//   };
+  const handleChange = value => {
+    setAddress(value);
+  };
 
-//   const handleSelect = value => {
-//     setAddress(value);
-//   };
-
-//   if (isLoaded && isSuccess) {
-//     return (
-//       <div>
-//         <PlacesAutocomplete value={address}
-//         onChange={handleChange}
-//         onSelect={handleSelect} >
-//           {({getInputProps, suggestions, getSuggestionItemProps, loading}) => (
-//             <div>
-//               <input {... getInputProps ({
-//                 placeholder: 'Enter...',
-//               })}
-//               />
-//               <div>
-//               {loading && <div>Loading... </div>}
-//               {suggestions.map((suggestion) => {
-//                 return (
-//                   <div {... getSuggestionItemProps(suggestion)}>
-//                     {suggestion.description}
-//                   </div>
-//                 )
-//               })}
-//               </div>
-//             </div>
-//           )}
-//       </div>
-//     )
-//   } else {
-//     return <div></div>
-//   }
-// }
+  const handleSelect = value => {
+    setAddress(value);
+  };
+  if (isLoaded && isSuccess) {
+    return <div>
+      <PlacesAutocomplete value={address} onChange={handleChange} onSelect={handleSelect}>
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input {...getInputProps({
+              placeholder: 'Enter Address ...'
+            })}/>
+          </div>
+        )}
+      </PlacesAutocomplete>
+    </div>;
+  } else {
+    return <div></div>;
+  }
+}
